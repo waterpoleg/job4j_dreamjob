6c4db4f549b2f1eb2bddfd26713ce4b501922898
Oleg Korneev <waterpoleg@gmail.com>
2022-04-07 18:26:58 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Candidate {
+    private int id;
+    private String name;
+    private String description;
+    private String created = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+
+    public Candidate(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id && name.equals(candidate.name)
+                && description.equals(candidate.description) && created.equals(candidate.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, created);
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Junior developer", "collections"));
+        candidates.put(2, new Candidate(2, "Middle developer", "spring"));
+        candidates.put(3, new Candidate(3, "Senior developer", "microservices architecture"));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
