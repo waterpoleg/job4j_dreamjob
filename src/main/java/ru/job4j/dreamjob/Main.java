2ca4a9ef6c10d4069575f7eb6649baab84f23ff9
Oleg Korneev <waterpoleg@gmail.com>
2022-04-16 21:00:55 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/Main.java
+++ src/main/java/ru/job4j/dreamjob/Main.java
@@ -1,8 +1,10 @@
 package ru.job4j.dreamjob;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@ThreadSafe
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
